a2ccb89181362a6220157b52d48eb3dd86ec7ce6
Bipin Maharjan <bopon.maharjan@gmail.com>
2020-01-13 09:47:57 +0545

    too many merge conflict

--- Elibrary/Payment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-	path('', views.getBook, name= 'getBook'),
-	path('getBook/', views.authorSplit, name ='authorSplit'), 
-    path('admin/', admin.site.urls),
-]
--- Elibrary/ReadBook/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import *
-
-admin.site.register(UserInfo)
-admin.site.register(Book)
-admin.site.register(PaymentDetail)
-admin.site.register(PendingPayment)
-admin.site.register(Review)
