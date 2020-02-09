2d266670855bf1df74bdc6d33b9f3b5947d23c47
Bipin Maharjan <bopon.maharjan@gmail.com>
2020-02-09 23:33:07 +0545

    Update urls.py

--- Elibrary/DELETE_API/urls.py
+++ Elibrary/DELETE_API/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "DELETE_API"
 
 urlpatterns = [
-  path('delete/id=<int:id>/',views.deleteBook,name="deleteBook"),
+  path('delete/book=<int:id>/',views.deleteBook,name="deleteBook"),
 
 ]
