cb01390c98eed4d8d5425b9dfd04620c923ee044
Bipin Maharjan <bopon.maharjan@gmail.com>
2020-02-10 14:42:25 +0545

    Removing Unused Model

--- Elibrary/ReadBook/admin.py
+++ Elibrary/ReadBook/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Book,Transaction,PendingTransaction,Review,UserInfo
+from .models import Book,Transaction,Review,UserInfo
 # Register your models here.
 admin.site.register(UserInfo)
 admin.site.register(Book)
 admin.site.register(Transaction)
-admin.site.register(PendingTransaction)
 admin.site.register(Review)
